76e28a081430720e99f42aceea6ba7e9ba5a9dbe
Parkwochang <whwh3560@gmail.com>
2023-08-22 19:02:06 +0900

    loop loading

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -8,7 +8,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="bg-zinc-900">
+    <html lang="en" className="">
       <body>{children}</body>
     </html>
   );
